12b87c9bc7645932add8c1f83254df98c873be2a
AdeebNqo <adeebnqo@gmail.com>
2014-03-15 15:45:31 +0200

    finished next(), need to do pad()

--- ga/src/BinStringGenerator.java
+++ ga/src/BinStringGenerator.java
@@ -1,10 +1,20 @@
 class BinStringGenerator{
 	int length;
+	int upperbound;
+	int current  = 0;
 	public BinStringGenerator(int len){
 		this.length = len;
+		upperbound = Math.pow(2,len);
 	}
 	public String next(){
-	
+		if (current<upperbound){
+			String encoding = Integer.toBinaryString(current);
+			++current;
+			(encoding.length()==length) ? encoding : pad(encoding);
+		}
+		else{
+			throw Exception("Cannot generate binary string. Reached upper bound.");
+		}
 	}
 	private String pad(String encoding){
 		return null;
